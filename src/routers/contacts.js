dbf65804d4029a91d51987b2d3b25c0e0716c364
Podolianyk <podolianik1988@gmail.com>
2024-07-23 19:32:18 +0200

    Initial commit for hw3-crud

---
+++ src/routers/contacts.js
@@ -0,0 +1,28 @@
+import express from 'express';
+import {
+  createContactController,
+  deleteContactController,
+  getContactByIdController,
+  getContactsController,
+  patchContactController,
+} from '../controllers/contacts.js';
+import { ctrlWrapper } from '../utils/ctrlWrapper.js';
+
+const router = express.Router();
+const jsonParser = express.json();
+
+router.get('/contacts', ctrlWrapper(getContactsController));
+
+router.get('/contacts/:contactId', ctrlWrapper(getContactByIdController));
+
+router.post('/contacts', jsonParser, ctrlWrapper(createContactController));
+
+router.patch(
+  '/contacts/:contactId',
+  jsonParser,
+  ctrlWrapper(patchContactController),
+);
+
+router.delete('/contacts/:contactId', ctrlWrapper(deleteContactController));
+
+export default router;
--- src/services/contacts.js
+++ src/services/contacts.js
@@ -4,3 +4,23 @@ export const getAllContacts = () => ContactsCollection.find();
 
 export const getContactById = (contactId) =>
   ContactsCollection.findById(contactId);
+
+export const createContact = (payload) => ContactsCollection.create(payload);
+
+export const updateContact = async (contactId, payload, options = {}) => {
+  const rawResult = await ContactsCollection.findByIdAndUpdate(
+    contactId,
+    payload,
+    { new: true, includeResultMetadata: true, ...options },
+  );
+
+  if (!rawResult || !rawResult.value) return null;
+
+  return {
+    student: rawResult.value,
+    isNew: Boolean(rawResult?.lastErrorObject?.upserted),
+  };
+};
+
+export const deleteContact = (contactId) =>
+  ContactsCollection.findByIdAndDelete(contactId);
