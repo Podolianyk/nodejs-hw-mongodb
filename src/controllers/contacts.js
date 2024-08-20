03d8ef58781cd437f6ad388aaa53bda904ae1adc
Podolianyk <podolianik1988@gmail.com>
2024-08-20 19:31:36 +0200

    update

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -71,7 +71,7 @@ export const createContactController = async (req, res) => {
 
   res.status(201).json({
     status: 201,
-    message: 'Successfully created a contact!',
+    message: 'Successfully created a contact!!',
     data: contact,
   });
 };
